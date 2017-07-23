8597a4781c081e1ac1031578e427ab8c12570a74
Angular CLI <angular-cli@angular.io>
2017-07-23 11:20:39 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularTestScssPage } from './app.po';
+
+describe('angular-test-scss App', () => {
+  let page: AngularTestScssPage;
+
+  beforeEach(() => {
+    page = new AngularTestScssPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
